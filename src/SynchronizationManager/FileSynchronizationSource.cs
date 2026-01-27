a1a1cd6d411d525ea0d32294274c3fd05823ced6
Карамов Артур <32008462+karamov-git@users.noreply.github.com>
2026-01-27 19:31:07 +0500

    implement FileSynchronizationSource

--- src/SynchronizationManager/FileSynchronizationSource.cs
+++ src/SynchronizationManager/FileSynchronizationSource.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace SynchronizationManager;
 
@@ -8,10 +11,30 @@ public interface ISynchronizationSource
     IEnumerable<Configuration> GetConfigurations();
 }
 
-public class FileSynchronizationSource : ISynchronizationSource
+public class FileSynchronizationSource(string pathToConfigurationFiles, ILogger logger) : ISynchronizationSource
 {
+    private const string ExtensionNameForConfigFiles = "fsc";
+
     public IEnumerable<Configuration> GetConfigurations()
     {
-        throw new NotImplementedException();
+        var allConfigFiles = Directory.GetFiles(pathToConfigurationFiles, $"*.{ExtensionNameForConfigFiles}", SearchOption.TopDirectoryOnly);
+
+        foreach (var configFileName in allConfigFiles)
+        {
+            Configuration? configuration = null;
+
+            try
+            {
+                var jsonConfig = File.ReadAllText(configFileName);
+                configuration = JsonConvert.DeserializeObject<Configuration>(jsonConfig);
+            }
+            catch (Exception e)
+            {
+                logger.LogError("Can't read config in file {0}, skipp. Inner exception {1}", configFileName, e);
+            }
+
+            if (configuration is not null)
+                yield return configuration;
+        }
     }
 }
---
+++ src/SynchronizationManagerTests/FileSynchronizationSourceTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace SynchronizationManagerTests;
+
+public class FileSynchronizationSourceTests
+{
+    [Test]
+    public void GetConfigurations_ReturnConfigsFromFile()
+    {
+    }
+
+    [Test]
+    public void GetConfigurations_ReturnEmptyConfigs_WhenFileNotExist()
+    {
+    }
+
+    [Test]
+    public void GetConfigurations_IgnoreConfig_WhenConfigIsIncorrect()
+    {
+    }
+}
