59978e64c78f2231f2b57d2db902e3a87b435d42
Карамов Артур Эдуардович <karamov@kontur>
2026-01-27 18:21:56 +0500

    smll fix

--- src/SynchronizationManager/SynchronizationProcess.cs
+++ src/SynchronizationManager/SynchronizationProcess.cs
@@ -5,34 +5,23 @@ using Microsoft.Extensions.Logging;
 
 namespace SynchronizationManager;
 
-public class SynchronizationProcess
+public class SynchronizationProcess(ISynchronizationSource synchronizationSource, ILogger logger, ISynchronizer synchronizer)
 {
-    private readonly ISynchronizationSource _synchronizationSource;
-    private readonly ISynchronizer _synchronizer;
-    private readonly ILogger _logger;
-
-    public SynchronizationProcess(ISynchronizationSource synchronizationSource, ILogger logger, ISynchronizer synchronizer)
-    {
-        _synchronizationSource = synchronizationSource;
-        _logger = logger;
-        _synchronizer = synchronizer;
-    }
-
     public async Task Start(CancellationToken cancellationToken)
     {
-        var configurations = _synchronizationSource.GetConfigurations();
+        var configurations = synchronizationSource.GetConfigurations();
 
         var process = new List<Synchronization>();
 
         foreach (var configuration in configurations)
         {
-            var synchronization = _synchronizer.Run(configuration);
+            var synchronization = synchronizer.Run(configuration);
             process.Add(synchronization);
         }
 
         while (cancellationToken.IsCancellationRequested == false)
         {
-            _logger.LogSynchronization(process);
+            logger.LogSynchronization(process);
         }
     }
 }
